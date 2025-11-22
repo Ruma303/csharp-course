2fa4e7ba3345e009b1e4bed8de1b4d2159dfa468
Ruma303 <matteoh8@hotmail.it>
2025-11-22 18:33:17 +0100

    ex favorite color

---
+++ Exercises/favorite-color/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+     Console.Write("Scrivi un valore scegliendo tra rosso, verde o blu: ");
+     string colore = Console.ReadLine();
+
+        switch (colore)
+        {
+            case "rosso":
+                Console.WriteLine("Il tuo colore preferito è il rosso");
+                break;
+            case "verde":
+                Console.WriteLine("Il tuo colore preferito è il verde");
+                break;
+            case "blu":
+                Console.WriteLine("Il tuo colore preferito è il blu");
+                break;
+            default:
+                Console.WriteLine("Colore sconosciuto");
+                break;
+        }
+    }
+}
