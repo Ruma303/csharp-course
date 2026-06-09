b835b8b60ca8f5fbe88ef7ab84e185193d96fe46
Ruma303 <matteoh8@hotmail.it>
2026-06-09 22:04:58 +0200

    aggiornamento versione a dotnet 10

--- Lessons/array/Program.cs
+++ Lessons/array/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    int[] x = new int[10] {1,2,3,45,76,0,8,53,3,5};
+
+    for (int i = 0; i < x.Length; i++)
+    {
+      Console.WriteLine(x[i]);
+    }
+
+    foreach (int value in x)
+    {
+        Console.WriteLine(value);
+    }
+  }
+}
